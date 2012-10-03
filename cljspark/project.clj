ee0f19bde0b1740012722ca3cbb7f29e6ad1a870
Mark Hamstra <markhamstra@gmail.com>
2012-10-03 13:30:57 -0700

    correct Maven identifiers for mesos-0.9.0-incubating

--- cljspark/project.clj
+++ cljspark/project.clj
@@ -5,7 +5,7 @@
                  ["sonatype" "http://oss.sonatype.org/content/groups/public/"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.spark-project/spark-core_2.9.2 "0.6.0-SNAPSHOT"]
-                 [org.apache/mesos "0.9.0"]]
+                 [org.apache.mesos/mesos "0.9.0-incubating"]]
   :min-lein-version "2.0.0"
   :aot [spark.api.clojure.FunctionFactory]
   :repl-options {
