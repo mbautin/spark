58f66221dd7194d09a96f396f34ffa771852b5db
Mark Hamstra <markhamstra@gmail.com>
2012-10-22 14:09:52 -0700

    bump to Spark 0.7.0-SNAPSHOT

--- cljspark/project.clj
+++ cljspark/project.clj
@@ -4,7 +4,7 @@
   :repositories [["typesafe" "http://repo.typesafe.com/typesafe/releases/"]
                  ["sonatype" "http://oss.sonatype.org/content/groups/public/"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.spark-project/spark-core_2.9.2 "0.6.0-SNAPSHOT"]
+                 [org.spark-project/spark-core_2.9.2 "0.7.0-SNAPSHOT"]
                  [org.apache.mesos/mesos "0.9.0-incubating"]]
   :min-lein-version "2.0.0"
   :aot [spark.api.clojure.FunctionFactory]
