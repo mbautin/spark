21a42f27097db4fbe5fb0cd64e6cfb53f7330177
Mark Hamstra <markhamstra@gmail.com>
2012-11-14 16:16:41 -0800

    Clojure hash-map and array-map conversions from tuple2

--- cljspark/src/spark/api/clojure/conversions.clj
+++ cljspark/src/spark/api/clojure/conversions.clj
@@ -1,6 +1,16 @@
 (in-ns 'spark.api.clojure.core)
 
-(defn scala-tuple2-to-map
+(defn- scala-tuple2-to-map
   "Convert a collection of Scala Tuple2 into a map where tuple2._1 is the key associated with value tuple2._2"
+  [tuple2s map-constructor]
+  (apply map-constructor (flatten (for [tuple2 tuple2s] (vector (._1 tuple2) (._2 tuple2))))))
+
+(defn scala-tuple2-to-hash-map
+  "Convert a collection of Scala Tuple2 into a hash-map where tuple2._1 is the key associated with value tuple2._2"
   [tuple2s]
-  (apply hash-map (flatten (for [tuple2 tuple2s] (vector (._1 tuple2) (._2 tuple2))))))
+  (scala-tuple2-to-map tuple2s hash-map))
+
+(defn scala-tuple2-to-array-map
+  "Convert a collection of Scala Tuple2 into an array-map where tuple2._1 is the key associated with value tuple2._2"
+  [tuple2s]
+  (scala-tuple2-to-map tuple2s array-map))
