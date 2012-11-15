2c7983bf603c4c28a233cdaf77cba1d539e6f69a
Mark Hamstra <markhamstra@gmail.com>
2012-11-14 17:13:57 -0800

    make scala-tuple2 conversions more idiomatic Clojure

--- cljspark/src/spark/api/clojure/conversions.clj
+++ cljspark/src/spark/api/clojure/conversions.clj
@@ -7,10 +7,10 @@
 
 (defn scala-tuple2-to-hash-map
   "Convert a collection of Scala Tuple2 into a hash-map where tuple2._1 is the key associated with value tuple2._2"
-  [tuple2s]
+  [& tuple2s]
   (scala-tuple2-to-map tuple2s hash-map))
 
 (defn scala-tuple2-to-array-map
   "Convert a collection of Scala Tuple2 into an array-map where tuple2._1 is the key associated with value tuple2._2"
-  [tuple2s]
+  [& tuple2s]
   (scala-tuple2-to-map tuple2s array-map))
