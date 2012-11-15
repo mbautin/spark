b430ae371b285f759b1c1eadc88539315f636cf8
Mark Hamstra <markhamstra@gmail.com>
2012-11-14 16:28:49 -0800

    use list instead of vector within tuple2 -> map conversion

--- cljspark/src/spark/api/clojure/conversions.clj
+++ cljspark/src/spark/api/clojure/conversions.clj
@@ -3,7 +3,7 @@
 (defn- scala-tuple2-to-map
   "Convert a collection of Scala Tuple2 into a map where tuple2._1 is the key associated with value tuple2._2"
   [tuple2s map-constructor]
-  (apply map-constructor (flatten (for [tuple2 tuple2s] (vector (._1 tuple2) (._2 tuple2))))))
+  (apply map-constructor (flatten (for [tuple2 tuple2s] (list (._1 tuple2) (._2 tuple2))))))
 
 (defn scala-tuple2-to-hash-map
   "Convert a collection of Scala Tuple2 into a hash-map where tuple2._1 is the key associated with value tuple2._2"
