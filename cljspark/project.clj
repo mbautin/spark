3447f58d1c5534959d378d118e15adf88c81919f
Mark Hamstra <markhamstra@gmail.com>
2012-11-24 15:56:31 -0800

    bump Clojure version to 1.5.0-beta1

--- cljspark/project.clj
+++ cljspark/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/markhamstra/spark/tree/dev/cljspark"
   :repositories [["typesafe" "http://repo.typesafe.com/typesafe/releases/"]
                  ["spray" "http://repo.spray.cc/"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-beta1"]
                  [org.spark-project/spark-core_2.9.2 "0.7.0-SNAPSHOT"]
                  [org.apache.mesos/mesos "0.9.0-incubating"]]
   :min-lein-version "2.0.0"
