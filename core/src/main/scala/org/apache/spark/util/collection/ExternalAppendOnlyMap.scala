5dae307f551fa2646e4e8748f9e9f84639f58c89
Mark Hamstra <markhamstra@gmail.com>
2014-03-14 11:15:29 -0700

    Removed unnecessary divergence from Apache source

--- core/src/main/scala/org/apache/spark/util/collection/ExternalAppendOnlyMap.scala
+++ core/src/main/scala/org/apache/spark/util/collection/ExternalAppendOnlyMap.scala
@@ -189,7 +189,6 @@ private[spark] class ExternalAppendOnlyMap[K, V, C](
     } finally {
       // Partial failures cannot be tolerated; do not revert partial writes
       writer.close()
-      _diskBytesSpilled += writer.bytesWritten
     }
 
     currentMap = new SizeTrackingAppendOnlyMap[K, C]
